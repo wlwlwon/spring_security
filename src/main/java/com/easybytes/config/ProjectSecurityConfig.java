b4dd0d31fe78b04121642020f9084393fe907e7e
wlwlwon <ryanwon@naver.com>
2022-02-20 23:49:47 +0900

    userDetails

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -1,50 +1,48 @@
 package com.easybytes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-/**
- * Default configurations which will secure all the requests
- */
+        http.authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated().antMatchers("/notices")
+                .permitAll().antMatchers("/contact").permitAll().and().formLogin().and().httpBasic();
 
-        /*
-         * http .authorizeRequests() .anyRequest().authenticated() .and()
-         * .formLogin().and() .httpBasic();
-         */
+    }
 
-        /**
-         * Custom configurations as per our requirement
-         */
+    //    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities
+//                 ("admin").and(). withUser("user").password("12345").authorities("read").and()
+//                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
 
-        /*
-         * http .authorizeRequests() .antMatchers("/myAccount").authenticated()
-         * .antMatchers("/myBalance").authenticated()
-         * .antMatchers("/myLoans").authenticated()
-         * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
-         * .antMatchers("/contact").permitAll() .and() .formLogin().and() .httpBasic();
-         */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
 
-        /**
-         * Configuration to deny all the requests
-         */
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user1);
+        auth.userDetailsService(userDetailsService);
+    }
 
-        /*
-         * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
-         * .httpBasic();
-         */
-
-        /**
-         * Configuration to permit all the requests
-         */
-
-        http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
-                .httpBasic();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
